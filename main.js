a14a26dd46cba7a4f6d1409032049713a03df6be
Dima <dmitrijs.visnakovs@inbox.lv>
2023-09-14 11:05:05 +0300

    POST+add new post

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const btn = document.querySelector('button');
+const btn = document.querySelector('.btn-get-posts');
+const btnAddPost = document.querySelector('.btn-add-post');
 const container = document.querySelector('.container');
 
 function getPosts(cb){
@@ -17,6 +18,52 @@ xhr.addEventListener('error', () => {
 
 xhr.send();
 }
+
+function createPost(body, cb){
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "https://jsonplaceholder.typicode.com/posts"); 
+    
+    xhr.addEventListener("load", () => {
+        const response = JSON.parse(xhr.responseText)
+        cb(response)
+    });
+
+    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+    
+    xhr.addEventListener('error', () => {
+        console.log('error')
+    })
+    
+    xhr.send(JSON.stringify(body));
+}
+
+function cardTemplate(post){
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    const title = document.createElement('h5');
+    title.classList.add('card-title');
+    title.textContent = post.title;
+    const article = document.createElement('p');
+    article.classList.add('card-text');
+    article.textContent = post.body;
+    cardBody.appendChild(title);
+    cardBody.appendChild(article);
+    card.appendChild(cardBody);
+    return card;
+}
+
+function renderPosts(response){
+    const fragment= document.createDocumentFragment(); 
+    response.forEach(post => {
+        const card = cardTemplate(post);
+        fragment.appendChild(card);
+    });
+    container.appendChild(fragment);
+}
+
 
 btn.addEventListener('click', e => {
     getPosts(response => {
@@ -42,6 +89,20 @@ btn.addEventListener('click', e => {
     container.appendChild(fragment);
 });
 });
+
+btnAddPost.addEventListener('click', (e) =>{
+    const newPost = {
+      title: 'foo',
+      body: 'bar',
+      userOd: 1,  
+    }
+    createPost(newPost, (response) => {
+        console.log(response)
+        const card = cardTemplate(response);
+        container.insertAdjacentElement('afterbegin', card);
+        console.log(card);
+    })
+})
 
 
 
